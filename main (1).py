fd2e4518684abb72df54fc66aa350ea23b1f3454
Manisachin <144360745+Manisachin@users.noreply.github.com>
2023-10-10 16:33:47 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,38 @@
-def isleapyear(year):
-  if (year % 4 == 0 and year % 100 !=0)or year % 400 == 0:
-    return True
-  else:
-    return False
+class BankAccount:
 
-year=int(input("Enter the year :"))
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-if isleapyear(year):
-  print("{} is a leap year.".format(year))
-else:
-  print("{} is not a leap year.".format(year))
+
+  def deposit(self,amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited:₹{}. New balance:₹{}".format(amount,
+                                                   self.__account_balance))
+    else:
+      print("Invalid deposit amount. Please deposit a positive amount.")
+
+
+  def withdraw(self,amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw:₹{}.New balance:₹{}".format(amount,
+                                                 self.__account_balance))
+    else:
+       print("Invalid withdrawl amount of insufficient balance.")
+
+
+  def display_balance(self):
+      print("Account balance for {} (Account #{}):{}".format(self.__account_holder_name, self.__account_number,self.__account_balance))
+
+
+account = BankAccount(account_number="123456789",
+                      account_holder_name="sachin",
+                      initial_balance=9000.0)
+
+account.display_balance()
+account.deposit(5000.0)
+account.withdraw(3000.0)
+account.display_balance()
